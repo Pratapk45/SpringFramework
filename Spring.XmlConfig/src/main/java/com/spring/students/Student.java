501bfdf43b4e005feae8169609ec1e481611f111
ABcom <ABcom@DESKTOP-IINQP9N>
2026-08-12 19:21:22 +0530

    "Adding Spring XmlConfig Project "

---
+++ Spring.XmlConfig/src/main/java/com/spring/students/Student.java
@@ -0,0 +1,28 @@
+package com.spring.students;
+
+public class Student {
+	
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Student() {
+		System.out.println("Object created...");
+	}
+
+	public void display() {
+		System.out.println("Id : "+id+" Name :"+name);
+	}
+}
