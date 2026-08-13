dd94dfc256c4919babb24f3fbad90ae68a490d88
ABcom <ABcom@DESKTOP-IINQP9N>
2026-08-13 20:10:53 +0530

    "Updating"

--- Spring.XmlConfig/src/main/java/com/spring/students/MainApp.java
+++ Spring.XmlConfig/src/main/java/com/spring/students/MainApp.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
